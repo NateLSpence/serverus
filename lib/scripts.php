10f6a474c0a86b0c3824944d76b72dbf665113b3
NateLSpence <NateLSpence@gmail.com>
2014-02-19 17:27:09 -0800

    Add styling for topics list

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd51bbfe4630ca32745f4c9c19a5752db');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '78f8993a407e5bf04391ba7860e461bb');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
