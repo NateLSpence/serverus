b2a98c5da9a8c8d39e711a251e7a410b89286e56
NateLSpence <NateLSpence@gmail.com>
2014-03-27 19:28:36 -0700

    Tweak general CSS. Fix top border issue, add drop shadows.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '962fa01c4227ad65d05916bebfb1d0e5');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ddb58651b4b259b5544823ef94dcf370');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
