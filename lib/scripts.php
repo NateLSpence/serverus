f9d782be18833c41231f7a38b0e7336eb0ac058d
NateLSpence <NateLSpence@gmail.com>
2014-04-10 15:13:55 -0700

    Centralize variables in bootstrap LESS. Tweaks to bbpress's CSS. Color scheme is now controlled by a small set of variables, located in variables.less, and every other color in the scheme is derived from those.
    The bbpress css tweaks were to add control in Serverus's files, as some of the
    styles were still in bbpress's hands.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '60499a822f7250ce620d4092283ccca2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8115a3f06fc1d79482fb0386c29af86b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
