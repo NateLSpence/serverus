7855ef4cf3bc28ef244116328c7402901e2cdbab
NateLSpence <NateLSpence@gmail.com>
2014-04-10 16:38:31 -0700

    Add -muted variables to the color scheme.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fba4c8d4c245e2536f354535ee6b052c');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd166b9f4a8115b87b8a89abacc48de53');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
