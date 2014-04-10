c28703c439c6dcbffc7daeaafabd0d60a218940f
NateLSpence <NateLSpence@gmail.com>
2014-04-10 16:02:35 -0700

    Tweak profile page CSS so it matches theme colors/bootstrap.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8115a3f06fc1d79482fb0386c29af86b');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fba4c8d4c245e2536f354535ee6b052c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
