973654048016c2f3742ba98b495295eda8293ed2
NateLSpence <NateLSpence@gmail.com>
2014-04-09 17:30:34 -0700

    Begin adjustment of LESS variables. Centralize. Test new color scheme.

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f3124785ebf9a1c109aef230f0633f7e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '60499a822f7250ce620d4092283ccca2');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
